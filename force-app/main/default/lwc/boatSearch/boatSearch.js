3e1d9b14bdd12db5d341b0c24bff3814b9770b74
Chris <christianniro18@gmail.com>
2023-12-07 15:43:08 +0100

    first commit

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,41 @@
+/**
+ * @description       : 
+ * @author            : CHristian Niro
+ * @group             : 
+ * @last modified on  : 03-30-2023
+ * @last modified by  : 
+**/
+import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class BoatSearch extends NavigationMixin(LightningElement) {
+    isLoading = false;
+    
+    // Handles loading event
+    handleLoading() {
+        this.isLoading = true;
+    }
+    
+    // Handles done loading event
+    handleDoneLoading() {
+        this.isLoading = false;
+    }
+    
+    // Handles search boat event
+    // This custom event comes from the form
+    searchBoats(event) {
+        let boatTypeId = event.detail.boatTypeId;
+        this.template.querySelector('c-boat-search-results').searchBoats(boatTypeId);
+        this.handleDoneLoading();
+    }
+    
+    createNewBoat() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Boat__c',
+                actionName: 'new'
+            }
+        });        
+    }
+}
